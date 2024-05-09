08100e74052dac7481898913fddc6c1ad7cf507a
Tamirat <amenguda@gmail.com>
2024-05-09 23:06:13 +0300

    Add admin dashBoards

--- Chest_Xray_Diagnosis_backend/base/admin.py
+++ Chest_Xray_Diagnosis_backend/base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Patient, Doctor, FeedBack, Disease, Prediction, Image
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(FeedBack)
+admin.site.register(Disease)
+admin.site.register(Prediction)
+admin.site.register(Image)
