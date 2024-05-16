c0e492323578bb41a6cb591f18176475ccb55270
Tamirat <amenguda@gmail.com>
2024-05-16 23:59:24 +0300

    Refactor views and serializers
    This commit refactors the views and serializers in the application to improve code organization and readability. Changes include:
    
    - Reorganizing imports and removing unused imports.
    - Rearranging class definitions and removing duplicate class definitions.
    - Updating view and serializer logic to handle exceptions and errors more gracefully.
    - Adding docstrings and comments for better code documentation.
    - Fixing typos and improving variable names for clarity.
    
    These changes enhance the maintainability and readability of the codebase.

--- Chest_Xray_Diagnosis_backend/base/views.py
+++ Chest_Xray_Diagnosis_backend/base/views.py
@@ -11,14 +11,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
 from .models import Image, Prediction, Disease
 from .serializers import ( 
-    PredictionSerializer, 
     DiseaseSerializer,
     EmailValidationOnForgotPasswordSerializer,
-    ProfileUpdateSerializer,
-    DoctorAddSerializer,
     PatientSerializer,
-    Imageser,
     UserSerializer,
+    PredictionSerializer,
+    DoctorProfileSerializer,
+    ImageSerializer
 )
 from tensorflow.keras.models import load_model
 import tensorflow as tf
@@ -31,26 +30,42 @@ from rest_framework.decorators import api_view, parser_classes, permission_class
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from .models import User, Doctor
-
-
+from .models import User 
+from .models import DoctorProfile
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
 import cv2 as cv
 import numpy as np
 from rest_framework import generics, status
 from rest_framework.response import Response
-from .models import Prediction  # Import the Prediction model from your Django application
+from .models import Prediction 
 from tensorflow.keras.models import load_model
+from rest_framework_simplejwt.tokens import OutstandingToken, TokenError
+from rest_framework.permissions import IsAdminUser
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import DoctorProfile
+from .serializers import DoctorProfileSerializer
+from django.contrib.auth import logout
+from django.http import JsonResponse
 
-# Define disease names
+
 DISEASE_NAMES = ['Cardiomegaly', 'Emphysema', 'Effusion', 'Hernia', 'Nodule', 'Pneumothorax', 'Atelectasis',
                  'Pleural_Thickening', 'Mass', 'Edema', 'Consolidation', 'Infiltration', 'Fibrosis', 'Pneumonia']
 
+from .models import Prediction, Patient
+from keras.preprocessing import image
 class Image_views(generics.CreateAPIView):
     
-    serializer_class = Imageser
+    serializer_class = PredictionSerializer
 
     def post(self, request, *args, **kwargs):
         image_data = request.FILES.get('image')
+        patient_id = request.data.get('patient')  
         if not image_data:
             return Response({'error': 'No image provided'}, status=status.HTTP_400_BAD_REQUEST)
         
@@ -58,142 +73,156 @@ class Image_views(generics.CreateAPIView):
             return Response({'error': 'Invalid image format. Supported formats: PNG, JPEG'}, status=status.HTTP_400_BAD_REQUEST)
         
         try:
-            img = cv.imdecode(np.fromstring(image_data.read(), np.uint8), cv.IMREAD_COLOR)
-            img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Convert image to grayscale
-            img_resized = cv.resize(img_gray, (128, 128))
-            img_array = img_resized / 255.0
-            img_array = np.expand_dims(img_array, axis=(0, -1))  # Add channel dimension
+            patient = Patient.objects.get(pk=patient_id)
+            print(patient)
             
+            img = cv.imdecode(np.fromstring(image_data.read(), np.uint8), cv.IMREAD_COLOR)
+            # img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Convert image to grayscale
+            # img_resized = cv.resize(img, (1, 224, 224, 1))
+            # img_array = img_resized / 255.0
+            # img_array = np.expand_dims(img_array, axis=(0, 1))  # Add channel dimension
+            img = cv.resize(img, (224,224))
+            x = image.img_to_array(img)
+            x = x.reshape((1,) + x.shape)  
+            x = x / 255.0  
+            x = (x - x.mean()) / x.std()  
             # Load the Keras model
-            keras_model_path = "model/le_tame_model_yehew_esti_eneyew_2.h5"
+            keras_model_path = "model/densenet_with_out_sequential.h5"
             keras_model = load_model(keras_model_path)
             
             # Make prediction using the loaded model
-            prediction = keras_model.predict(img_array)
+            prediction = keras_model.predict(x)
             print(prediction)
-            predicted_label = DISEASE_NAMES[np.argmax(prediction)]
-            print(predicted_label)
+            diseases = DISEASE_NAMES[np.argmax(prediction)]
             
-            # Save prediction result to the database
-            new_prediction = Prediction(image=image_data, predicted_label=predicted_label)
+            # Save prediction result to the database with patient data
+            new_prediction = Prediction(image=image_data, diseases=diseases, patient=patient)
             new_prediction.save()
             
-            return Response({'prediction': predicted_label})
+            return Response({'prediction': diseases})
             
+        except Patient.DoesNotExist:
+            return Response({'error': 'Patient not found'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = TokenObtainPairSerializer
-
-class LogoutView(APIView):
-    permission_classes = [IsAuthenticated]
-    def post(self, request):
-        logout(request)
-        return Response(status=status.HTTP_205_RESET_CONTENT)
+        
+class UploadImage(generics.CreateAPIView):
+    serializer_class = ImageSerializer
+    queryset = Image.objects.all()
 
 class SignUpView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
-        request.data['is_doctor'] = True  # Fix typo
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            try:
+                user = serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            
+            except Exception as e:
+                print(f"Error saving user: {e}")
+                return Response({"detail": "An error occurred while creating the user."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class ProfileUpdateView(generics.UpdateAPIView):
+class LogoutView(APIView):
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh_token")
+            token = OutstandingToken(refresh_token)
+            token.blacklist()
+            return Response({"detail": "Successfully logged out."}, status=status.HTTP_200_OK)
+        except TokenError as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+def logout_view(request):
     permission_classes = [IsAuthenticated]
-    serializer_class = ProfileUpdateSerializer
-    queryset = User.objects.all()
+    logout(request)
+    return JsonResponse({'message': 'Logged out successfully'}, status=200)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):   
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        token['user_role'] = user.get_user_role  
+
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        data['user_role'] = self.user.get_user_role
+        print(data)
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response_data = {
+            'access_token': str(serializer.validated_data['access']),
+            'refresh_token': str(serializer.validated_data['refresh']),
+            'user_role': str(serializer.validated_data['user_role']),
+            'email': str(serializer.validated_data['email']),
+            'username': str(serializer.validated_data['username']),
+       
+        }
+        print(response_data)
+        return Response(response_data)
+
+
+
+class DoctorList(generics.ListAPIView):
+    serializer_class = DoctorProfileSerializer
+    queryset = DoctorProfile.objects.all()
+
+class VerifyDoctorView(APIView):
+    def post(self, request, doctor_id):
+        try:
+            doctor_profile = DoctorProfile.objects.get(pk=doctor_id)
+
+            doctor_profile.verify_doctor()
+
+            return Response({"detail": "Doctor verified successfully."}, status=status.HTTP_200_OK)
+
+        except DoctorProfile.DoesNotExist:
+            return Response({"detail": "Doctor profile not found."}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            print(f"Error verifying doctor: {e}")
+            return Response({"detail": "An error occurred while verifying the doctor."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class UserListView(APIView):
     def get(self, request, *args, **kwargs):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-class AllDataView(APIView):
-    def get(self, request, format=None):
-        predictions = Prediction.objects.all()
-        serializer = PredictionSerializer(predictions, many=True)
-        return Response(serializer.data)
-
-class PredictionView(APIView):
-    parser_classes = [MultiPartParser, FormParser]
-    
-    def post(self, request, format=None):
-        data = request.data
-        request_image = data.get("image")
-        if not request_image:
-            return Response("No image provided", status=status.HTTP_400_BAD_REQUEST)
+        users = User.objects.all()        
+        total_users = User.get_total_users()
+        total_doctors = User.get_total_doctors()       
+        serializer = UserSerializer(users, many=True)       
+        data = {
+            'total_users': total_users,
+            'total_doctors': total_doctors,
+            'users': serializer.data
+        }
         
-        # Process the image
-        try:
-            image = cv.imdecode(np.fromstring(request_image.read(), np.uint8), cv.IMREAD_COLOR)
-            image_size = image.shape[:2]
-            image_name = data.get("imageName")
-            scanned = data.get("scanned", "").lower() == "true"
-            
-            verification_image = cv.resize(image, (128, 128))
-            verification_image = np.reshape(verification_image, (1, 224, 224, 3))
-            input_data = np.array(verification_image, dtype=np.float32)
-            interpreter.set_tensor(input_details[0]["index"], input_data)
-            interpreter.invoke()
-            output_data = interpreter.get_tensor(output_details[0]["index"])[0][0]
-            if output_data > 0.2:
-                return Response("Invalid image", status=status.HTTP_400_BAD_REQUEST)
+        return Response(data, status=status.HTTP_200_OK)
+    
 
-            # Rotate image if needed
-            rotated = False
-            if image.shape[0] > image.shape[1]:
-                image = cv.rotate(image, cv.ROTATE_90_COUNTERCLOCKWISE)
-                rotated = True
-
-            # Resize and process the image
-            image = cv.resize(image, (512, 512))
-            image_name = 'xRay'
-            predicted_image, predicted_diseases, severity = self.segment_image(image=image)
-
-            # Save prediction
-            prediction = Prediction(
-                imageName=image_name,
-                scanned=scanned,
-                image=request_image,
-                severity=severity,
-            )
-            prediction.save()
-
-            # Rotate image back if rotated
-            if rotated:
-                predicted_image = cv.rotate(predicted_image, cv.ROTATE_90_CLOCKWISE)
-
-            # Resize and save the predicted image
-            predicted_image = cv.resize(predicted_image, image_size[::-1])
-            _, buf = cv.imencode(".jpg", predicted_image)
-            content = ContentFile(buf.tobytes())
-            prediction.predictedImage.save(image_name, content)
-
-            # Create or get disease objects
-            disease_objects = [Disease.objects.get_or_create(name=name)[0] for name in predicted_diseases]
-
-            # Add diseases to the prediction
-            prediction.diseases.add(*disease_objects)
-
-            serializer = PredictionSerializer(prediction)
-            return Response(serializer.data)
-
-        except Exception as e:
-            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def segment_image(self, image):
-        # Image segmentation and disease prediction logic
-        return predicted_image, predicted_diseases, severity
+class PredictionListView(generics.ListAPIView):
+    serializer_class = PredictionSerializer
+    queryset = Prediction.objects.all()
+    
 
 class DockorAddView(generics.CreateAPIView):
-    serializer_class = DoctorAddSerializer
+    serializer_class = DoctorProfileSerializer
 
 class PateintAddView(generics.CreateAPIView):
     serializer_class = PatientSerializer
@@ -206,11 +235,22 @@ class PateintAddView(generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response({"message": "User and Student created successfully."}, status=status.HTTP_201_CREATED)
-class ProfileUpdateView(generics.UpdateAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = ProfileUpdateSerializer
-    queryset = User.objects.all()
 
+class AllPatient(generics.ListCreateAPIView):
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
+
+
+
+class DoctorProfileCreateAPIView(APIView):
+    def post(self, request, format=None):
+        serializer = DoctorProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    
+    
 class EmailValidationOnForgotPasswordView(generics.CreateAPIView):
     serializer_class = EmailValidationOnForgotPasswordSerializer
 
@@ -244,53 +284,6 @@ class EmailValidationOnForgotPasswordView(generics.CreateAPIView):
         )
 
         return Response({"message": "Reset link sent to the provided email address."}, status=status.HTTP_200_OK)
-
-
-
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from .serializers import UserSerializer, DoctorSerializer  # Import the DoctorSerializer
-from .models import Doctor  # Import the Doctor model
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .serializers import UsersSerializer
-from .models import User
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .serializers import UserSerializer, DoctorAddSerializer
-from .models import User, Doctor
-
-class DashboardAPI(APIView):
-    def get(self, request, *args, **kwargs):
-        all_users = User.objects.all()
-        doctors = Doctor.objects.all()  # Get queryset of doctors
-        doctor_count = doctors.count()
-        admin_count = User.objects.filter(is_superuser=True).count()
-        current_month_users = User.objects.filter(date_joined__month=timezone.now().month).count()
-        last_month_users = User.objects.filter(date_joined__month=timezone.now().month - 1).count()
-        recent_users = User.objects.order_by('-date_joined')[:5]
-
-        user_serializer = UserSerializer(all_users, many=True)
-        doctor_serializer = DoctorAddSerializer(doctors, many=True)  # Pass queryset to serializer
-
-        data = {
-            'all_users': user_serializer.data,
-            'doctor_count': doctor_count,
-            'admin_count': admin_count,
-            'current_month_users': current_month_users,
-            'last_month_users': last_month_users,
-            'recent_users': recent_users,
-            'doctors': doctor_serializer.data,  # Include serialized doctors data in response
-        }
-        return Response(data, status=status.HTTP_200_OK)
-
-
-
-
-
 
 
 
