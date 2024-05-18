da0e1896ad37b328c25cad782e16c9b2e23cb00b
Tamirat <amenguda@gmail.com>
2024-05-18 21:25:24 +0300

    Add SignIn component with form handling and Redux integration
    - Implement SignIn component using Flowbite React components for UI.
    - Manage form state with useState.
    - Use useSelector to get loading and error state from Redux store.
    - Use useDispatch to dispatch sign-in actions (signInStart, signInSuccess, signInFailure).
    - Handle form submission with handleSubmit to make an API call for user authentication.
    - Display loading spinner on submit button during API call.
    - Show error messages using Alert component on failure.
    - Provide navigation to sign-up page using Link component.

---
+++ frontends/src/pages/SignIn.jsx
@@ -0,0 +1,99 @@
+import { Alert, Button, Card, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+
+
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password) {
+      return dispatch(signInFailure('Please fill all the fields'));
+    }
+    try {
+      dispatch(signInStart());
+      const res = await fetch('http://127.0.0.1:8000/api/token/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+      }
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate('/');
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
+  return (
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        <Card className='flex-1 bg-gradient-to-br p-12 from-slate-50 to-white via-slate-100'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label value='Your email' />
+              <TextInput
+                type='text'
+                placeholder='name@company.com'
+                id='username'
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput
+                type='password'
+                placeholder='**********'
+                id='password'
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+            className='bg-gradient-to-l w-28 lg:ml-72 from-green-800 to-teal-800 via-cyan-600'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign In'
+              )}
+            </Button>
+        
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Dont Have an account?</span>
+            <Link to='/sign-up' className='text-blue-500'>
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
+        </Card>
+      </div>
+    </div>
+  );
+}
